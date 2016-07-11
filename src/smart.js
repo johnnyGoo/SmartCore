90a9bdb471b0d94087c98aa03116e6db79555a2c
johnnyGoo <157781745@qq.com>
2016-07-11 23:26:38 +0800

    update export

--- src/smart.js
+++ src/smart.js
@@ -10,17 +10,4 @@ import Sound from './Sound'
 import Tween from './Tween'
 import Url from './Url'
 import Video from './Video'
-var Smart={};
-Smart._=_;
-Smart.Animations=Animations;
-Smart.Css=Css;
-Smart.Event=Event;
-Smart.Utils=Utils;
-Smart.EventDispatcher=EventDispatcher;
-Smart.Loader=PxLoader;
-Smart.Device=Device;
-Smart.Sound=Sound;
-Smart.Tween=Tween;
-Smart.Url=Url;
-Smart.Video=Video;
-export default Smart;
+export {_,Animations,Css,Event,Utils,EventDispatcher,PxLoader,Device,Sound,Tween,Url,Video}
